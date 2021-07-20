802fcc23743c91b4b78f2c2e5c2943ed6c65e971
Harshit Nigdikar <harshitnigdikar@gmail.com>
2021-07-20 20:28:19 +0530

    Added Paint Houses

---
+++ Dynamic Programming/PaintHouses.java
@@ -0,0 +1,38 @@
+// 1. You are given a number n, representing the number of houses.
+// 2. In the next n rows, you are given 3 space separated numbers representing the cost of painting nth house with red or blue or green color.
+// 3. You are required to calculate and print the minimum cost of painting all houses without painting any consecutive house with same color.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[][] arr = new int[n][3];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        long[][]dp = new long[n][3];
+        
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        
+        for(int i = 1; i < arr.length; i++){
+            dp[i][0] = arr[i][0] + Math.min(dp[i-1][1], dp[i- 1][2]);
+            dp[i][1] = arr[i][1] + Math.min(dp[i-1][0], dp[i- 1][2]);
+            dp[i][2] = arr[i][2] + Math.min(dp[i-1][0], dp[i- 1][1]);
+        }
+        
+        long ans = Math.min(dp[n-1][0] , Math.min(dp[n-1][1],dp[n-1][2]));
+        System.out.println(ans);
+
+    }
+}
