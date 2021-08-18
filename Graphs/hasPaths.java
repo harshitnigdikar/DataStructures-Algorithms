0c54b2a10293c416aae55a5249b808f02479f667
Harshit Nigdikar <harshitnigdikar@gmail.com>
2021-08-18 20:12:17 +0530

    Has Paths

---
+++ Graphs/hasPaths.java
@@ -0,0 +1,64 @@
+// 1. You are given a graph, a src vertex and a destination vertex.
+// 2. You are required to find if a path exists between src and dest. If it does, print true 
+//      otherwise print false.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt){
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for(int i = 0; i < vtces; i++){
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for(int i = 0; i < edges; i++){
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      int src = Integer.parseInt(br.readLine());
+      int dest = Integer.parseInt(br.readLine());
+
+      boolean[]visited = new boolean[vtces];
+      boolean path = hasPath(graph, src, dest, visited);
+      
+      System.out.println(path);
+    }
+    
+    public static boolean hasPath(ArrayList<Edge>[]graph, int src, int dest, boolean[]visited){
+        
+        if(src == dest){
+            return true;
+        }
+        visited[src] = true;
+        for(Edge edge : graph[src]){
+            if(visited[edge.nbr] == false){
+                boolean hasNbrPath = hasPath(graph, edge.nbr, dest, visited);
+                if(hasNbrPath == true){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
